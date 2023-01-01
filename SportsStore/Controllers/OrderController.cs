b3e117a15da9f16cb3ca33c20cf02b16aa0d09ec
Skip Sailors <ssailors@wpc-edi.com>
2023-01-01 00:40:09 -0800

    listin 9-20

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -1,10 +1,43 @@
-﻿namespace SportsStore.Controllers
-{
-    using Microsoft.AspNetCore.Mvc;
-    using Models;
+﻿namespace SportsStore.Controllers;
 
-    public class OrderController: Controller
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+public class OrderController : Controller
+{
+    private readonly Cart cart;
+
+    private readonly IOrderRepository repository;
+
+    public OrderController(
+        IOrderRepository repoService,
+        Cart cartService)
     {
-        public ViewResult Checkout() => View(new Order());
+        repository = repoService;
+        cart = cartService;
+    }
+
+    public ViewResult Checkout()
+    {
+        return View(new Order());
+    }
+
+    [HttpPost]
+    public IActionResult Checkout(Order order)
+    {
+        if (!cart.Lines.Any())
+        {
+            ModelState.AddModelError("", "Sorry, your cart is empty");
+        }
+
+        if (ModelState.IsValid)
+        {
+            order.Lines = cart.Lines.ToArray();
+            repository.SaveOrder(order);
+            cart.Clear();
+            return RedirectToPage("/Completed", new { orderId = order.OrderID });
+        }
+
+        return View();
     }
 }
